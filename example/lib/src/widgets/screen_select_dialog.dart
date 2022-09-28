77ca530189830f8e17c65318f9b036176aa880e2
kyudong <kyudong0422@gmail.com>
2022-09-28 10:20:25 +0900

    first

---
+++ example/lib/src/widgets/screen_select_dialog.dart
@@ -0,0 +1,327 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+// ignore: must_be_immutable
+class ScreenSelectDialog extends Dialog {
+  ScreenSelectDialog() {
+    Future.delayed(Duration(milliseconds: 100), () {
+      _getSources();
+    });
+    _subscriptions.add(desktopCapturer.onAdded.stream.listen((source) {
+      _sources[source.id] = source;
+      _stateSetter?.call(() {});
+    }));
+
+    _subscriptions.add(desktopCapturer.onRemoved.stream.listen((source) {
+      _sources.remove(source.id);
+      _stateSetter?.call(() {});
+    }));
+
+    _subscriptions.add(desktopCapturer.onNameChanged.stream.listen((source) {
+      _sources[source.id] = source;
+      _stateSetter?.call(() {});
+    }));
+
+    _subscriptions
+        .add(desktopCapturer.onThumbnailChanged.stream.listen((source) {
+      _sources[source.id] = source;
+      _stateSetter?.call(() {});
+    }));
+  }
+  final Map<String, DesktopCapturerSource> _sources = {};
+  SourceType _sourceType = SourceType.Screen;
+  DesktopCapturerSource? _selected_source;
+  final List<StreamSubscription<DesktopCapturerSource>> _subscriptions = [];
+  StateSetter? _stateSetter;
+  Timer? _timer;
+
+  void _ok(context) async {
+    _timer?.cancel();
+    _subscriptions.forEach((element) {
+      element.cancel();
+    });
+    Navigator.pop<DesktopCapturerSource>(context, _selected_source);
+  }
+
+  void _cancel(context) async {
+    _timer?.cancel();
+    _subscriptions.forEach((element) {
+      element.cancel();
+    });
+    Navigator.pop<DesktopCapturerSource>(context, null);
+  }
+
+  Future<void> _getSources() async {
+    try {
+      var sources = await desktopCapturer.getSources(types: [_sourceType]);
+      sources.forEach((element) {
+        print(
+            'name: ${element.name}, id: ${element.id}, type: ${element.type}');
+      });
+      _stateSetter?.call(() {
+        sources.forEach((element) {
+          _sources[element.id] = element;
+        });
+      });
+      _timer?.cancel();
+      _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+        desktopCapturer.updateSources(types: [_sourceType]);
+      });
+      return;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Center(
+          child: Container(
+        width: 640,
+        height: 560,
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Choose what to share',
+                      style: TextStyle(fontSize: 16, color: Colors.black87),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      child: Icon(Icons.close),
+                      onTap: () => _cancel(context),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                child: StatefulBuilder(
+                  builder: (context, setState) {
+                    _stateSetter = setState;
+                    return DefaultTabController(
+                      length: 2,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            constraints: BoxConstraints.expand(height: 24),
+                            child: TabBar(
+                                onTap: (value) => Future.delayed(
+                                        Duration(milliseconds: 300), () {
+                                      _sourceType = value == 0
+                                          ? SourceType.Screen
+                                          : SourceType.Window;
+                                      _getSources();
+                                    }),
+                                tabs: [
+                                  Tab(
+                                      child: Text(
+                                    'Entire Screen',
+                                    style: TextStyle(color: Colors.black54),
+                                  )),
+                                  Tab(
+                                      child: Text(
+                                    'Window',
+                                    style: TextStyle(color: Colors.black54),
+                                  )),
+                                ]),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: TabBarView(children: [
+                                Align(
+                                    alignment: Alignment.center,
+                                    child: Container(
+                                      child: GridView.count(
+                                        crossAxisSpacing: 8,
+                                        crossAxisCount: 2,
+                                        children: _sources.entries
+                                            .where((element) =>
+                                                element.value.type ==
+                                                SourceType.Screen)
+                                            .map((e) => Column(
+                                                  children: [
+                                                    Expanded(
+                                                        child: Container(
+                                                      decoration: (_selected_source !=
+                                                                  null &&
+                                                              _selected_source!
+                                                                      .id ==
+                                                                  e.value.id)
+                                                          ? BoxDecoration(
+                                                              border: Border.all(
+                                                                  width: 2,
+                                                                  color: Colors
+                                                                      .blueAccent))
+                                                          : null,
+                                                      child: InkWell(
+                                                        onTap: () {
+                                                          print(
+                                                              'Selected screen id => ${e.value.id}');
+                                                          setState(() {
+                                                            _selected_source =
+                                                                e.value;
+                                                          });
+                                                        },
+                                                        child:
+                                                            e.value.thumbnail !=
+                                                                    null
+                                                                ? Image.memory(
+                                                                    e.value
+                                                                        .thumbnail!,
+                                                                    scale: 1.0,
+                                                                    repeat: ImageRepeat
+                                                                        .noRepeat,
+                                                                  )
+                                                                : Container(),
+                                                      ),
+                                                    )),
+                                                    Text(
+                                                      e.value.name,
+                                                      style: TextStyle(
+                                                          fontSize: 12,
+                                                          color: Colors.black87,
+                                                          fontWeight: (_selected_source !=
+                                                                      null &&
+                                                                  _selected_source!
+                                                                          .id ==
+                                                                      e.value
+                                                                          .id)
+                                                              ? FontWeight.bold
+                                                              : FontWeight
+                                                                  .normal),
+                                                    ),
+                                                  ],
+                                                ))
+                                            .toList(),
+                                      ),
+                                    )),
+                                Align(
+                                    alignment: Alignment.center,
+                                    child: Container(
+                                      child: GridView.count(
+                                        crossAxisSpacing: 8,
+                                        crossAxisCount: 3,
+                                        children: _sources.entries
+                                            .where((element) =>
+                                                element.value.type ==
+                                                SourceType.Window)
+                                            .map((e) => Column(
+                                                  children: [
+                                                    Expanded(
+                                                        child: Container(
+                                                      decoration: (_selected_source !=
+                                                                  null &&
+                                                              _selected_source!
+                                                                      .id ==
+                                                                  e.value.id)
+                                                          ? BoxDecoration(
+                                                              border: Border.all(
+                                                                  width: 2,
+                                                                  color: Colors
+                                                                      .blueAccent))
+                                                          : null,
+                                                      child: InkWell(
+                                                        onTap: () {
+                                                          print(
+                                                              'Selected window id => ${e.value.id}');
+                                                          setState(() {
+                                                            _selected_source =
+                                                                e.value;
+                                                          });
+                                                        },
+                                                        child: e
+                                                                .value
+                                                                .thumbnail!
+                                                                .isNotEmpty
+                                                            ? Image.memory(
+                                                                e.value
+                                                                    .thumbnail!,
+                                                                scale: 1.0,
+                                                                repeat:
+                                                                    ImageRepeat
+                                                                        .noRepeat,
+                                                              )
+                                                            : Container(),
+                                                      ),
+                                                    )),
+                                                    Text(
+                                                      e.value.name,
+                                                      style: TextStyle(
+                                                          fontSize: 12,
+                                                          color: Colors.black87,
+                                                          fontWeight: (_selected_source !=
+                                                                      null &&
+                                                                  _selected_source!
+                                                                          .id ==
+                                                                      e.value
+                                                                          .id)
+                                                              ? FontWeight.bold
+                                                              : FontWeight
+                                                                  .normal),
+                                                    ),
+                                                  ],
+                                                ))
+                                            .toList(),
+                                      ),
+                                    )),
+                              ]),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: ButtonBar(
+                children: <Widget>[
+                  MaterialButton(
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                    onPressed: () {
+                      _cancel(context);
+                    },
+                  ),
+                  MaterialButton(
+                    color: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Share',
+                    ),
+                    onPressed: () {
+                      _ok(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/src/native/desktop_capturer_impl.dart
@@ -0,0 +1,172 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+
+import '../desktop_capturer.dart';
+import 'utils.dart';
+
+class DesktopCapturerSourceNative extends DesktopCapturerSource {
+  DesktopCapturerSourceNative(
+      this._id, this._name, this._thumbnailSize, this._type);
+  factory DesktopCapturerSourceNative.fromMap(Map<dynamic, dynamic> map) {
+    var sourceType = (map['type'] as String) == 'window'
+        ? SourceType.Window
+        : SourceType.Screen;
+    var source = DesktopCapturerSourceNative(map['id'], map['name'],
+        ThumbnailSize.fromMap(map['thumbnailSize']), sourceType);
+    source.thumbnail = map['thumbnail'] as Uint8List;
+    return source;
+  }
+  Function(String name)? onNameChanged;
+  Function()? onRemoved;
+  Function()? onThumbnailChanged;
+
+  Uint8List? _thumbnail;
+  String _name;
+  final String _id;
+  final ThumbnailSize _thumbnailSize;
+  final SourceType _type;
+
+  set thumbnail(Uint8List? value) {
+    _thumbnail = value;
+    onThumbnailChanged?.call();
+  }
+
+  set name(String name) {
+    _name = name;
+  }
+
+  @override
+  String get id => _id;
+
+  @override
+  String get name => _name;
+
+  @override
+  Uint8List? get thumbnail => _thumbnail;
+
+  @override
+  ThumbnailSize get thumbnailSize => _thumbnailSize;
+
+  @override
+  SourceType get type => _type;
+}
+
+class DesktopCapturerNative extends DesktopCapturer {
+  DesktopCapturerNative._internal() {
+    EventChannel('FlutterWebRTC/desktopSourcesEvent')
+        .receiveBroadcastStream()
+        .listen(eventListener, onError: errorListener);
+  }
+
+  static final DesktopCapturerNative instance =
+      DesktopCapturerNative._internal();
+
+  final Map<String, DesktopCapturerSourceNative> _sources = {};
+
+  void eventListener(dynamic event) async {
+    final Map<dynamic, dynamic> map = event;
+    switch (map['event']) {
+      case 'desktopSourceAdded':
+        final source = DesktopCapturerSourceNative.fromMap(map);
+        if (_sources[source.id] == null) {
+          _sources[source.id] = source;
+          onAdded.add(source);
+        }
+        break;
+      case 'desktopSourceRemoved':
+        final source = _sources[map['id'] as String];
+        if (source != null) {
+          _sources.remove((source) => source.id == map['id']);
+          onRemoved.add(source);
+        }
+        break;
+      case 'desktopSourceThumbnailChanged':
+        final source = _sources[map['id'] as String];
+        if (source != null) {
+          try {
+            source.thumbnail = map['thumbnail'] as Uint8List;
+            onThumbnailChanged.add(source);
+          } catch (e) {
+            print('desktopSourceThumbnailChanged: $e');
+          }
+        }
+        break;
+      case 'desktopSourceNameChanged':
+        final source = _sources[map['id'] as String];
+        if (source != null) {
+          source.name = map['name'];
+          onNameChanged.add(source);
+        }
+        break;
+    }
+  }
+
+  void errorListener(Object obj) {
+    if (obj is Exception) {
+      throw obj;
+    }
+  }
+
+  @override
+  Future<List<DesktopCapturerSource>> getSources(
+      {required List<SourceType> types, ThumbnailSize? thumbnailSize}) async {
+    final response = await WebRTC.invokeMethod(
+      'getDesktopSources',
+      <String, dynamic>{
+        'types': types.map((type) => desktopSourceTypeToString[type]).toList(),
+        if (thumbnailSize != null) 'thumbnailSize': thumbnailSize.toMap(),
+      },
+    );
+    if (response == null) {
+      throw Exception('getDesktopSources return null, something wrong');
+    }
+    for (var source in response['sources']) {
+      var sourceType = (source['type'] as String) == 'window'
+          ? SourceType.Window
+          : SourceType.Screen;
+      var desktopSource = DesktopCapturerSourceNative(
+          source['id'],
+          source['name'],
+          ThumbnailSize.fromMap(source['thumbnailSize']),
+          sourceType);
+      if (source['thumbnail'] != null) {
+        desktopSource.thumbnail = source['thumbnail'] as Uint8List;
+      }
+      _sources[desktopSource.id] = desktopSource;
+    }
+    return _sources.values.toList();
+  }
+
+  @override
+  Future<bool> updateSources({required List<SourceType> types}) async {
+    final response = await WebRTC.invokeMethod(
+      'updateDesktopSources',
+      <String, dynamic>{
+        'types': types.map((type) => desktopSourceTypeToString[type]).toList(),
+      },
+    );
+    if (response == null) {
+      throw Exception('updateSources return null, something wrong');
+    }
+    return response['result'] as bool;
+  }
+
+  Future<Uint8List?> getThumbnail(DesktopCapturerSourceNative source) async {
+    final response = await WebRTC.invokeMethod(
+      'getDesktopSourceThumbnail',
+      <String, dynamic>{
+        'sourceId': source.id,
+        'thumbnailSize': {
+          'width': source.thumbnailSize.width,
+          'height': source.thumbnailSize.height
+        }
+      },
+    );
+    if (response == null || !response is Uint8List?) {
+      throw Exception('getDesktopSourceThumbnail return null, something wrong');
+    }
+    return response as Uint8List?;
+  }
+}
